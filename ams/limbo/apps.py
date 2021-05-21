ee90c3f55bb0270ba7b9a718b136b4b3f7ffdf9f
pritik24 <77440703+pritik24@users.noreply.github.com>
2021-05-21 23:01:25 +0530

    Delete ams directory

--- ams/limbo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LimboConfig(AppConfig):
-    name = 'limbo'
--- ams/limbo/urls.py
+++
@@ -1,9 +0,0 @@
-from limbo import views
-from django.urls import include, path, re_path
-
-app_name='basic'
-
-urlpatterns = [
-    path('login/',views.index,name='login'),
-
-]
